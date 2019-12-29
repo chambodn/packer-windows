f949f217dea6a413c1d3eb9dabf998e63a9a34fb
chambodn <chambodn@skysoft-atm.com>
2019-12-29 15:11:37 +0100

    try with powershell provisioner

---
+++ scripts/01-install-chocolatey.ps1
@@ -0,0 +1,14 @@
+#Requires -RunAsAdministrator
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# install chocolatey if not installed
+if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
+  Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+$Packages = 'firefox', 'vcbuildtools', 'notepadplusplus', 'devcon.portable'
+
+ForEach ($PackageName in $Packages){
+    choco install $PackageName -y
+}
